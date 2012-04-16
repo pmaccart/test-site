8da26164f6ca05fd2da00cfcb73dd9e3c72abdb6
Phil MacCart <pmaccart@alumni.nd.edu>
2012-04-15 23:07:47 -0500

    update port

--- www/app.js
+++ www/app.js
@@ -37,5 +37,5 @@ console.log(routes.post);
 app.get('/', routes.index);
 app.get('/post', routes.post);
 
-app.listen(3000);
+app.listen(80);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
